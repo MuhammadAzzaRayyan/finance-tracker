9fef681f9ab34e19d1ba5a52ec348b3a7a83beb6
Your Name <you@example.com>
2026-06-17 19:30:38 +0700

    chore: remove redundant root files

--- run-local.ps1
+++
@@ -1,9 +0,0 @@
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Force
-Push-Location $PSScriptRoot
-if (-Not (Test-Path .venv)) {
-    python -m venv .venv
-}
-& .\.venv\Scripts\Activate.ps1
-pip install -r requirements.txt
-python app.py
-Pop-Location
